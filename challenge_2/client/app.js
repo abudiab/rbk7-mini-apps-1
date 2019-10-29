ae413051264f1b33d5ec886e84236465522a6917
abudiab <mahmoud.w.diab@gmail.com>
2019-10-29 19:13:38 +0200

    barely anything

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,42 @@
+//need post request
+// need get request
+//on submit click get the data and save it in a variable 
+//create a post request after ur process the data
+
+//on convert click receive from server and show the data after processes
+//create a get request to the server
+$('#submit').on('click', function() {
+
+    var jsonData = $('#txtarea').val();
+    console.log(jsonData);
+
+    var csvData;
+
+    $.ajax({
+        url: '/upload_json',
+        type: 'POST',
+        data: jsonData,
+        success: console.log('sent from client'),
+        error: console.log('error not sent from client')
+    });
+
+    $.ajax({
+        url: '/upload_json',
+        type: 'GET',
+        data: csvData, //change this
+        success: console.log('received to client'),
+        error: console.log('error not received to client')
+    });
+
+});
 
 
+
+
+      
+        
+  
+    
+    
+    
+  
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,26 @@
+var express = require('express');
+var app = new express();
 
+app.use(express.static('client'));
+
+app.get('/', function (req, res) {
+    res.sendfile('index.html');
+});
+
+var received = 'gotcha json'
+var converted = 'herya csv'; //change this
+
+app.get('/upload_json', function (req, res) {
+    console.log(converted);
+    res.send(converted);
+});
+
+app.post('/upload_json', function (req, res) {
+    console.log(received);
+   res.send(received);
+});
+
+app.listen(3000, function(){
+    console.log("Listening on port 3000!");
+});
 
