58d21f5db097541d9d424d89aac425043ee0e3eb
abudiab <mahmoud.w.diab@gmail.com>
2019-10-31 00:21:51 +0200

    Attempt

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,26 @@
+//we don't import bcuz we linked the react in the html using cdn links
+
+//app class
+
+class App extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Checkout!</h1>
+                <h2></h2>
+            </div>
+        );
+    }
+}
 
 
+ReactDOM.render(<App />, document.getElementById('app'));
+  
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,6 @@
+var express = require('express');
+var app = express();
+var port = 8000;
+app.use(express.static('public'));
 
-
+app.listen(port, () => console.log(`Server listening on port ${port}`));
